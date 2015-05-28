df4783c6a7a6d5d39cd639db1f127919a598c5bb
marilenaK <marilena_klemmer@yahoo.de>
2015-05-28 17:49:43 +0200

    abfragen fertig

--- Aufgabe37/aufgabe/abfragen.py
+++ Aufgabe37/aufgabe/abfragen.py
@@ -13,10 +13,25 @@ def augsburg():
     
 #alle Nike-Vereine, welche schwarz als mindestens eine Vereinsfarbe haben
 def nikeSchwarz():
-    for item in client.vereine_db.fussball.find({'nike' : 'j', 'farben' : 'schwarz'}):
+    for item in client.vereine_db.fussball.find({"$and": {'nike' : 'j', 'farben': {"$in": ['schwarz']}}}):    
         print(item)
         
-#alle Nike-Vereine, welche weiss und gruuen als Vereinsfarbe haben
+#alle Nike-Vereine, welche weiss und gruen als Vereinsfarbe haben
 def  nikeWeissGruen():
-    for item in client.vereine_db.fussball.find({'nike' : 'j', 'farben' : ['weiss', 'gruen']}):
+    for item in client.vereine_db.fussball.find({"$and": {'nike' : 'j', "$and":[{'farben' : 'weiss'}, {'farben':'gruen'}]}}):
+        print(item)
+
+# alle Nike-Vereine, welche weiss oder gruen als Vereinsfarbe haben       
+def nikeWeissOrgruen():
+    for item in client.vereine_db.fussball.find({"$and": {"nike": 'j', "$or": [{'farben' : 'weiss'}, {'farben' : 'gruen'}]}}):
+        print(item)
+        
+#den Verein mit dem hoechsten Tabellenplatz
+def highestPlace():
+    for item in client.vereine_db.fussball.find({"$max": {"$Tabellenplatz"}}):
+        print(item)
+
+#alle Vereine, die nicht auf einem Abstiegsplatz stehen        
+def keinAbstieg():
+    for item in client.vereine_db.fussball.find({"$not": {'Tabellenplatz' : 17}}):
         print(item)
