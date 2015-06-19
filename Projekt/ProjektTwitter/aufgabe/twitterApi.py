833f901588f9b5ffb6372d59c279e8c0d1d84cef
marilenaK <marilena_klemmer@yahoo.de>
2015-06-19 12:41:29 +0200

    import funktioniert

--- Projekt/ProjektTwitter/aufgabe/twitterApi.py
+++ Projekt/ProjektTwitter/aufgabe/twitterApi.py
@@ -4,23 +4,24 @@ Created on 19.06.2015
 @author: marilena
 '''
 import twitter
+import simplejson
 
 def GetFollowerIDs(self, userid=None, cursor=-1, count = 10):
-   url = 'http://twitter.com/followers/ids.json'
-   parameters = {}
-   parameters['cursor'] = cursor
-   if userid:
-       parameters['user_id'] = userid
-   remaining = count
-   while remaining > 1:
-       remaining -= 1
-       json = self._FetchUrl(url, parameters=parameters)
-       try:
-           data = simplejson.loads(json)
-           self._CheckForTwitterError(data)
-       except twitterError:
-           break
-   return data
+    url = 'http://twitter.com/followers/ids.json'
+    parameters = {}
+    parameters['cursor'] = cursor
+    if userid:
+        parameters['user_id'] = userid
+        remaining = count
+        while remaining > 1:
+            remaining -= 1
+            json = self._FetchUrl(url, parameters=parameters)
+            try:
+                data = simplejson.loads(json)
+                self._CheckForTwitterError(data)
+                except twitterError:
+                    break
+                return data
 
 def main():
     api = twitter.Api(consumer_key='XXXX',
