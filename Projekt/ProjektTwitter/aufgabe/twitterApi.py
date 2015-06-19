2ed1776c514c9a5e9f20b74ced398ee9a350380e
Vladimir Malikov <v.malikov.hamburg@gmail.com>
2015-06-19 13:19:35 +0200

    Klassen

--- Projekt/ProjektTwitter/aufgabe/twitterApi.py
+++ Projekt/ProjektTwitter/aufgabe/twitterApi.py
@@ -3,8 +3,9 @@ Created on 19.06.2015
 
 @author: marilena
 '''
+
 import twitter
-import simplejson
+import json
 
 def GetFollowerIDs(self, userid=None, cursor=-1, count = 10):
     url = 'http://twitter.com/followers/ids.json'
@@ -17,18 +18,18 @@ def GetFollowerIDs(self, userid=None, cursor=-1, count = 10):
             remaining -= 1
             json = self._FetchUrl(url, parameters=parameters)
             try:
-                data = simplejson.loads(json)
+                data = json.loads(json)
                 self._CheckForTwitterError(data)
-                except twitterError:
+            except twitter.TwitterError:
                     break
-                return data
+            return data
 
 def main():
-    api = twitter.Api(consumer_key='XXXX',
+    api = twitter.api(consumer_key='XXXX',
                       consumer_secret='XXXXX',
                       access_token_key='XXXXX',
                       access_token_secret='XXXXXX')
     user=api.GetUser(screen_name="XXXXXX")
     count = 100 # you can find optimum value by trial & error
-    while(#users not empty):
-        users=api.GetFollowerIDs(user,count)
+    while True:
+        api.GetFollowerIDs(user,count)
